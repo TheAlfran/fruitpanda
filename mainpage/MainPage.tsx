984ad67201d6f92f3ed6d0c28daa553048814290
TheAlfran <alfranexx25@gmail.com>
2023-10-03 22:29:23 +0800

    main page ui

--- mainpage/MainPage.tsx
+++ mainpage/MainPage.tsx
@@ -1,11 +1,17 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import { MainContainer } from './mainstyle'
+import { MainContainer, MainSearch, MainTitle, MainTitleContainer } from './mainstyle'
+
+
+
 
 export default function MainPage() {
   return (
     <MainContainer>
-      
+      <MainTitleContainer>
+        <MainTitle>Explore you favorite fruits</MainTitle>
+        <MainSearch placeholder='Search fruits' placeholderTextColor="wheat"></MainSearch>
+      </MainTitleContainer>
     </MainContainer>
   );
 }
--- mainpage/mainstyle.ts
+++ mainpage/mainstyle.ts
@@ -4,7 +4,34 @@ import styled from "styled-components/native";
 
 export const MainContainer = styled.View`
   flex: 1;
-  align-items: center;
-  justify-content: center;
   background-color: wheat;
 `;
+
+export const MainTitleContainer = styled.View`
+background-color: #fff;
+justify-content: center;
+align-items: center;
+`;
+
+export const MainTitle = styled.Text`
+font-size: 24px;
+font-weight: bold;
+color: #b30000;
+padding: 30px;
+position: relative;
+right: 21px;
+top: 20px;
+`;
+
+export const MainSearch = styled.TextInput`
+border-width: 1px;
+border-color: #FFA726;
+border-radius: 15px;
+height: 30px;
+width: 90%;
+margin-bottom: 20px;
+padding: 5px;
+padding-left: 10px;
+
+`;
+
