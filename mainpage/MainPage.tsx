c590a99b2b392e1d2a8d49a69e134827206294cb
TheAlfran <alfranexx25@gmail.com>
2023-10-03 23:08:14 +0800

    buttons for categories

--- mainpage/MainPage.tsx
+++ mainpage/MainPage.tsx
@@ -1,17 +1,42 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, TouchableOpacity } from 'react-native'
 import React from 'react'
-import { MainContainer, MainSearch, MainTitle, MainTitleContainer } from './mainstyle'
+import { CategoryButtons, CategoryContainer, MainContainer, MainSearch, MainTitle, MainTitleContainer, ParentCategoryContainer } from './mainstyle'
 
 
 
 
 export default function MainPage() {
+
+  const items = [
+    { id: 1, text: 'Citrus' },
+    { id: 2, text: 'Berries' },
+    { id: 3, text: 'Tropical' },
+    { id: 4, text: 'Stone' },
+    { id: 5, text: 'Exotic' },
+    { id: 6, text: 'Melons' },
+    { id: 7, text: 'Apples' },
+    { id: 8, text: 'Grapes' },
+    { id: 9, text: 'T-Citrus' },
+    // Add more items as needed
+  ];
+  
   return (
     <MainContainer>
       <MainTitleContainer>
         <MainTitle>Explore you favorite fruits</MainTitle>
         <MainSearch placeholder='Search fruits' placeholderTextColor="wheat"></MainSearch>
       </MainTitleContainer>
+      <ParentCategoryContainer>
+        <CategoryContainer>
+            <ScrollView horizontal>
+            {items.map((item) => (
+              <CategoryButtons key={item.id}>
+                <Text style={{color: "wheat"}}>{item.text}</Text>
+              </CategoryButtons>
+            ))}
+          </ScrollView>
+        </CategoryContainer>
+      </ParentCategoryContainer>
     </MainContainer>
   );
 }
--- mainpage/mainstyle.ts
+++ mainpage/mainstyle.ts
@@ -4,7 +4,7 @@ import styled from "styled-components/native";
 
 export const MainContainer = styled.View`
   flex: 1;
-  background-color: wheat;
+  background-color: #fff;
 `;
 
 export const MainTitleContainer = styled.View`
@@ -33,5 +33,33 @@ margin-bottom: 20px;
 padding: 5px;
 padding-left: 10px;
 
+`;
+
+export const ParentCategoryContainer = styled.View`
+justify-content: center;
+align-items: center;
+
+`;
+
+export const CategoryContainer = styled.View`
+width: 90%;
+height: 50px;
+justify-content: center;
+align-items: center;
+background-color: #fff;
+
+`;
+
+export const CategoryButtons = styled.TouchableOpacity`
+border-width: 1px;
+border-color: #FFA726;
+border-bottom-color: #fff;
+border-top-color: #fff;
+border-left-color: #fff;
+height: 30px;
+width: 80px;
+justify-content: center;
+align-items: center;
+position: relative;
 `;
 
