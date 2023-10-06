82ae8c7ff77105b5f0bd72eeb4d725cdf90aad82
TheAlfran <alfranexx25@gmail.com>
2023-10-06 14:59:46 +0800

    new design

--- src/@core/page/Main/MainPage.tsx
+++ src/@core/page/Main/MainPage.tsx
@@ -1,11 +1,12 @@
 import { FlatList, View, Text, ScrollView, TouchableOpacity } from 'react-native'
 import React, { useState, useEffect } from 'react'
-import { CategoryButtons, CategoryContainer, MainContainer, MainSearch, MainTitle, MainTitleContainer, ParentCategoryContainer, ParentProductContainer, ProductButton, ProductContainer, ProductImage, SaleContainer } from './mainstyle'
+import { CategoryButtons, CategoryContainer, MainContainer, MainSearch, MainTitle, MainTitleContainer, ParentCategoryContainer, ParentProductContainer, ProductButton, SaleButton, ProductContainer, ProductImage, SaleContainer, TextSale, PriceTextContainer } from './mainstyle'
 import axios from 'axios';
 
 type Product = {
+  id: number;
   attributes: {
-    id: number;
+
     name: string;
     quantity: number;
     price: number;
@@ -18,7 +19,6 @@ type Product = {
         }
       }
     }
-
   };
 };
 
@@ -27,7 +27,7 @@ export default function MainPage() {
   const fetchProducts = async () => {
     try {
       const response = await axios.get("http://192.168.1.4:1337/api/products?populate=image");
-      
+
       return response.data.data;
 
     } catch (error) {
@@ -36,8 +36,32 @@ export default function MainPage() {
     }
   };
 
+  const fetchProducts1 = async () => {
+    try {
+      const response = await axios.get("http://192.168.1.4:1337/api/products?filters[id][$in][1]=7&filters[id][$in][0]=1&filters[id][$in][4]=9&populate=image");
+      console.log("data", response.data.data)
+      return response.data.data;
+
+    } catch (error) {
+      console.error('Error fetching products:', error);
+      return [];
+    }
+  };
+
+  const [products1, setProducts1] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetchProducts1()
+      .then((response) => {
+        setProducts1(response);
+      })
+      .catch((error) => {
+        // Handle any errors here
+      });
+  }, []);
 
   const [products, setProducts] = useState<Product[]>([]);
+
   useEffect(() => {
     fetchProducts()
       .then((response) => {
@@ -47,6 +71,9 @@ export default function MainPage() {
         // Handle any errors here
       });
   }, []);
+
+
+
 
   const items = [
     { id: 1, text: 'Citrus' },
@@ -65,7 +92,7 @@ export default function MainPage() {
     <MainContainer>
       <MainTitleContainer>
         <MainTitle>Explore your favorite fruits</MainTitle>
-        <MainSearch placeholder='Search fruits' placeholderTextColor="wheat"></MainSearch>
+        <MainSearch placeholder='Search fruits' placeholderTextColor="#FFA726"></MainSearch>
       </MainTitleContainer>
       <ParentCategoryContainer>
         <CategoryContainer>
@@ -79,33 +106,53 @@ export default function MainPage() {
         </CategoryContainer>
       </ParentCategoryContainer>
       <SaleContainer>
+        <TextSale>50% off Items</TextSale>
+        <FlatList
+          data={products1} // Use products1 data for this FlatList
+          scrollEnabled
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          keyExtractor={(item: any, index) => {
+            return item.id.toString() || index.toString();
+          }}
+          renderItem={({ item: { attributes }, }) => (
 
+            <SaleButton key={attributes.id}>
+              <ProductImage source={{ uri: `http://192.168.1.4:1337${attributes?.image.data[0].attributes.url}` }} />
+              <Text style={{ textTransform: "uppercase" }}>{attributes.name}</Text>
+              <PriceTextContainer>
+                <Text style={{textDecorationLine: 'line-through'}}>₱{attributes.price}</Text>
+                <Text> / ₱{attributes.price * 0.5}</Text>
+              </PriceTextContainer>
+            </SaleButton>
+          )}
+        />
       </SaleContainer>
-      
-        <ProductContainer>
-          <FlatList
-            data={products}
-            scrollEnabled
-            showsVerticalScrollIndicator={false}
-            keyExtractor={(item: any, index) => {
-              return item.id.toString() || index.toString()
-            }}
-            numColumns={2}
-            key={'_'}
-            renderItem={({ item: { attributes } }) => (
-              <>
-                <ParentProductContainer>
-                  <ProductButton key={attributes.id}>
-                    <ProductImage source={{ uri: `http://192.168.1.4:1337${attributes?.image.data[0].attributes.url}` }} />
-                    <Text>{attributes.name}</Text>
-                  </ProductButton>
-                </ParentProductContainer>
-              </>
-            )
-            }
-          />
-        </ProductContainer>
-      
+      <ProductContainer>
+        <FlatList
+          data={products}
+          scrollEnabled
+          showsVerticalScrollIndicator={false}
+          keyExtractor={(item: any, index) => {
+            return item.id.toString() || index.toString()
+          }}
+          numColumns={2}
+          key={'_'}
+          renderItem={({ item: { attributes }, }) => {
+            return <>
+              <ParentProductContainer>
+                <ProductButton key={attributes.id}>
+                  <ProductImage source={{ uri: `http://192.168.1.4:1337${attributes?.image.data[0].attributes.url}` }} />
+                  <Text style={{ textTransform: "uppercase" }}>{attributes.name}</Text>
+                  <Text>₱{attributes.price}</Text>
+                </ProductButton>
+              </ParentProductContainer>
+            </>
+          }
+          }
+        />
+      </ProductContainer>
+
     </MainContainer>
   );
 }
