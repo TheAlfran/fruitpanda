7b401bd1800d594e0d933e757139d1f7a50d6794
TheAlfran <alfranexx25@gmail.com>
2023-10-06 11:29:48 +0800

    added color to the header

--- src/@core/page/Main/mainstyle.ts
+++ src/@core/page/Main/mainstyle.ts
@@ -1,15 +1,15 @@
 import styled from "styled-components/native";
+import {primary, secondary, tertiary} from '../../theme/theme';
 
 
 
 export const MainContainer = styled.View`
   flex: 1;
   background-color: #fff;
-  
 `;
 
 export const MainTitleContainer = styled.View`
-background-color: #fff;
+background-color: ${secondary};
 justify-content: center;
 align-items: center;
 `;
@@ -17,7 +17,7 @@ align-items: center;
 export const MainTitle = styled.Text`
 font-size: 24px;
 font-weight: bold;
-color: #b30000;  
+color: ${tertiary};  
 padding: 30px;
 position: relative;
 right: 21px;
@@ -26,13 +26,14 @@ top: 20px;
 
 export const MainSearch = styled.TextInput`
 border-width: 1px;
-border-color: #FFA726;
-border-radius: 15px;
-height: 30px;
+border-color: ${secondary};
+border-radius: 9px;
+height: 40px;
 width: 90%;
 margin-bottom: 20px;
 padding: 5px;
 padding-left: 10px;
+background-color: #fff;
 
 `;
 
@@ -41,7 +42,9 @@ justify-content: center;
 align-items: center;
 position: relative;
 bottom: 10px;
-
+border-bottom-right-radius: 19px;
+border-bottom-left-radius: 19px;
+background-color: ${secondary};
 `;
 
 export const CategoryContainer = styled.View`
@@ -49,16 +52,16 @@ width: 90%;
 height: 50px;
 justify-content: center;
 align-items: center;
-background-color: #fff;
+background-color: ${secondary};
 
 `;
 
 export const CategoryButtons = styled.TouchableOpacity`
 border-width: 1px;
-border-color: #FFA726;
-border-bottom-color: #fff;
-border-top-color: #fff;
-border-left-color: #fff;
+border-color: ${secondary};
+border-bottom-color: ${secondary};
+border-top-color: ${secondary};
+border-left-color: ${secondary};
 height: 30px;
 width: 80px;
 justify-content: center;
@@ -69,10 +72,21 @@ position: relative;
 export const SaleContainer = styled.View`
 height: 25%;
 width: 100%;
-background-color: #fff;
-`;
-export const ParentProductContainer = styled.View`
+align-items: center;
+margin-bottom: 20px;
+border-radius: 10px;
 
+`;
+
+export const SaleBorder = styled.View`
+border-width: 1px;
+border-color: #000;
+border-radius: 20px;
+height: 180px;
+width: 90%;
+`;
+
+export const ParentProductContainer = styled.View`
 flex: 1;
 background-color: #fff;
 `;
@@ -81,14 +95,14 @@ export const ProductContainer = styled.View`
 background-color: #fff;
 flex: 1;
 width: 100%;
+height: 150px;
 padding-left: 20px;
 padding-right: 20px;
-justify-content: center;
 `;
 
 export const ProductButton = styled.TouchableOpacity`
 border-width: 1px;
-border-color: black; 
+border-color: ${secondary}; 
 border-radius: 20px;  
 width: 90%;    
 height: 120px;
@@ -103,6 +117,7 @@ align-items: center;
 export const ProductImage = styled.Image`
 width: 50px;
 height: 50px;
+margin-bottom: 3px;
 `;
 
 
