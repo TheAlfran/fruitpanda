4781e7c17c43524131d4b7cebd3e31757f8e22c6
TheAlfran <alfranexx25@gmail.com>
2023-10-17 15:56:29 +0800

    Major changes: getting the uid for the user logged

--- src/@core/hooks/Profile/profileUtils.tsx
+++ src/@core/hooks/Profile/profileUtils.tsx
@@ -1,19 +1,34 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import AsyncStorage from "@react-native-async-storage/async-storage";  // Import AsyncStorage
 import { User } from '../Global/userList';
 
-export function useUserData() {
-  const [userData, setUserData] = useState<User[]>([]);
+export function useUserData() {  // Remove userId from parameters
+  const [userData, setUserData] = useState<User | null>(null);
+  const [userId, setUserId] = useState<string | null>(null);  // Add this line
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    axios.get('http://192.168.1.77:1337/api/users?populate=image')
-      .then(response => {
-        setUserData(response.data);
-      })
-      .catch(error => {
-        console.error('There was an error!', error);
-      });
+    const fetchUserId = async () => {
+      const storedUserId = await AsyncStorage.getItem("userId");
+      setUserId(storedUserId);
+    };
+
+    fetchUserId();
   }, []);
 
-  return userData;
+  useEffect(() => {
+    if (userId) {  // Only run if userId is not null
+      axios.get(`http://192.168.1.77:1337/api/users/${userId}?populate=image`)
+        .then(response => {
+          setUserData(response.data);
+        })
+        .catch(error => {
+          console.error('There was an error!', error);
+          setError(error);
+        });
+    }
+  }, [userId]);  // Add userId to dependency array
+
+  return {userData, error};
 }
