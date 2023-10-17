82675cdf697bf525f93ee6b69d5a87a430574cf4
TheAlfran <alfranexx25@gmail.com>
2023-10-17 15:58:27 +0800

    changed the secondary and other color for the theme

--- src/@core/theme/theme.ts
+++ src/@core/theme/theme.ts
@@ -1,8 +1,8 @@
 export const text = "#FFC966";       // Text color
 export const background = "#fff";    // Background color
 export const primary = "#b30000";    // Primary color
-export const secondary = "#FFA726";  // Secondary color
+export const secondary = "#D70F64";  // Secondary color
 export const tertiary = "wheat";     // Tertiary color
-export const quaternary = "#FF726B"; // Quaternary color (matching the theme)
-export const quinary = "rgba(255, 213, 79, 0.7)";    // Quinary color (matching the theme)
+export const quaternary = "#f279ac"; // Quaternary color (matching the theme)
+export const quinary = "#f279ac";    // Quinary color (matching the theme)
 export const senary = "#FFA500";     // Senary color (matching the theme)
