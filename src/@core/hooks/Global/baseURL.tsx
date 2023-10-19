f8c1b14fd01397e1af4dffd28eaa7efe4fc47562
TheAlfran <alfranexx25@gmail.com>
2023-10-20 00:38:05 +0800

    changed ip

--- src/@core/hooks/Global/baseURL.tsx
+++ src/@core/hooks/Global/baseURL.tsx
@@ -1,2 +1,2 @@
-export const API_URL = "http://192.168.1.77:1337/api/products";
-export const BASE_URL = "http://192.168.1.77:1337";
+export const API_URL = "http://192.168.1.7:1337/api/products";
+export const BASE_URL = "http://192.168.1.7:1337";
